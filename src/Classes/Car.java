fe674d166a99c68d8b6a3707c98a1776ed9e5c5c
JD-Flores <90649355+JD-Flores@users.noreply.github.com>
2023-06-21 14:43:12 -0400

    initial classes

---
+++ src/Classes/Car.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author Juan Diego
+ */
+public class Car {
+    
+    private int id;
+    
+    public Car (int id) {
+        this.id = id;
+    }
+}
---
+++ src/Classes/Procesador.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author Juan Diego
+ */
+public class Procesador extends Thread {
+    
+    private int processingTime;
+    
+    public Procesador (int processingTimeInMs){
+        this.processingTime = processingTimeInMs;
+    }
+    
+    public void race(Car bugattiCar, Car lamborghiniCar){
+        
+    }
+    
+}
